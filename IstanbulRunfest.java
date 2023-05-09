a7d551d41bfc5f62c0dd7c6e3d2353fad2b1570f
mylmzsc <48759142+mylmzsc@users.noreply.github.com>
2023-05-09 19:08:53 +0300

    Last update IstanbulRunfest.java

--- IstanbulRunfest.java
+++ IstanbulRunfest.java
@@ -1,79 +1,81 @@
 package com.company;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class IstanbulRunfest {
 
-    public static void fastest1(Integer[] given){
-        int lowestTime = given[0], indexOfFast = 0, indexOfSecondFast = 100,secondLowest= given[1];
-        int current;
+	public static void fastest1(Integer[] given, String[] names){
+		int lowestTime = given[0], indexOfFast = 0, indexOfSecondFast = 100,secondLowest= given[1];
+		int current;
 
-        for(int k =0; k < given.length ;k++){
-            current = given[k];
-            if(current < lowestTime){
-                secondLowest = lowestTime;
-                lowestTime = current;
-                indexOfFast = k;
-            }
-        }
-        for(int kk =0; kk < given.length ;kk++){
-            if(given[kk] == secondLowest)
-                indexOfSecondFast = kk;
-        }
-        System.out.println("Fastest runner's index is: " +indexOfFast);
-        System.out.println("He/She ran for: "+ lowestTime +" minutes");
+		for(int k =0; k < given.length ;k++){
+			current = given[k];
+			if(current < lowestTime){
+				secondLowest = lowestTime;
+				lowestTime = current;
+				indexOfFast = k;
+			}
+		}
+		for(int kk =0; kk < given.length ;kk++){
+			if(given[kk] == secondLowest)
+				indexOfSecondFast = kk;
+		}
+		System.out.println("Fastest runner's names is: " + names[indexOfFast]);
+		System.out.println("He/She ran for: "+ lowestTime +" minutes");
 
-        //System.out.println("Second Fastest runner's index is: " +indexOfSecondFast);
-        //System.out.println("He/She ran for: "+ secondLowest +" minutes");
+		//System.out.println("Second Fastest runner's index is: " +indexOfSecondFast);
+		//System.out.println("He/She ran for: "+ secondLowest +" minutes");
 
-    }
-    public static void fastest2(Integer[] given){
-        int lowestTime = given[0], indexOfFast = 0, indexOfSecondFast = 100,secondLowest= given[1];
-        int current;
+	}
+	public static void fastest2(Integer[] given, String[] names){
+		int lowestTime = given[0], indexOfFast = 0, indexOfSecondFast = 100,secondLowest= given[1];
+		int current;
 
-        for(int k =0; k < given.length ;k++){
-            current = given[k];
-            if(current < lowestTime){
-                secondLowest = lowestTime;
-                lowestTime = current;
-                indexOfFast = k;
-            }
-        }
-        for(int kk =0; kk < given.length ;kk++){
-            if(given[kk] == secondLowest)
-                indexOfSecondFast = kk;
-        }
-        //System.out.println("Fastest runner's index is: " +indexOfFast);
-        //System.out.println("He/She ran for: "+ lowestTime +" minutes");
+		Integer[] given2 = given;
+		Arrays.sort(given2);
+		secondLowest = given2[1];
 
-        System.out.println("Second Fastest runner's index is: " +indexOfSecondFast);
-        System.out.println("He/She ran for: "+ secondLowest +" minutes");
+		for(int kk =0; kk < given.length ;kk++){
+			if(given[kk] == secondLowest)
+				indexOfSecondFast = kk;
+		}
 
-    }
-    public static void main(String[] args) {
-        //Integer[] ty = new Integer[] {8,1,4,3,6,3,7};
-        //fastest(ty);
-        String nums;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Please provide the minutes of runners by entering a space between numbers: ");
-        nums = sc.nextLine();
-        int tt = 0;
+		System.out.println("Second Fastest runner's name is: " + names[indexOfSecondFast]);
+		System.out.println("He/She ran for: "+ given2[1] +" minutes");
 
-        String[] nums1 = nums.split(" ");
-        Integer[] nums2 = new Integer[nums1.length];
+	}
+	public static void main(String[] args) {
+		//Integer[] ty = new Integer[] {8,1,4,3,6,3,7};
+		//fastest(ty);
+		String nums,names;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please provide the name list of runners with {name1,name2.. } format: ");
+		names = sc.nextLine();
+		System.out.print("Please provide the minutes of runners with {min1,min2.. } format: ");
+		nums = sc.nextLine();
 
-        for (int y =0 ;y < nums1.length; y ++){
-             tt = Integer.parseInt(nums1[y]);
-             nums2[y] = tt;
-        }
-        fastest1(nums2);
-        fastest2(nums2);
+		int tt = 0;
 
-        sc.close();
+		nums = nums.substring(1, nums.length() - 1);
+		names = names.substring(1, names.length() - 1);
 
+		String[] nums1 = nums.split(",");
+		String[] names1 = names.split(",");
 
+		Integer[] nums2 = new Integer[nums1.length];
 
-    }
+		for (int y =0 ;y < nums1.length; y ++){
+			tt = Integer.parseInt(nums1[y]);
+			nums2[y] = tt;
+		}
+		fastest1(nums2,names1);
+		fastest2(nums2,names1);
+
+		sc.close();
+		//trial data {Ibrahim,Berke,Metin,İrem,Yigit,Melis} {272,479,278,329,445,402}
+
+	}
 }
