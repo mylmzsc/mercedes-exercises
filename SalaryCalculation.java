5204a0ff3586f39815b1b4c4176cb2fad9ea1184
mylmzsc <48759142+mylmzsc@users.noreply.github.com>
2023-05-09 19:20:54 +0300

    Update SalaryCalculation.java

--- SalaryCalculation.java
+++ SalaryCalculation.java
@@ -12,18 +12,26 @@ public class SalaryCalculation {
 			else{
 				if (hw<=40){
 					total = hw * bp;
-					System.out.println("Total is: "+ total);
+					System.out.println("Total salary is: "+ total);
 				}
 				else if(hw >40){
 					total =  ((hw * bp) + ( hw - 40 ) * (1.5 * bp));
-					System.out.println("Total is: "+ total);
+					System.out.println("Total salary is: "+ total);
 				}
 			}
 	}
 
 	public static void main(String[] args){
+		System.out.println("Employee1 with Basepay:90.50 Hours Worked:35");
+		System.out.print("For Employee1 -> ");
 		totalPay(35, 90.50);
+		System.out.println("Employee2 with Basepay:120.50 Hours Worked:47");
+		System.out.print("For Employee2 -> ");
 		totalPay(47, 120.50);
+		System.out.println("Employee3 with Basepay:150.00 Hours Worked:73");
+		System.out.print("For Employee3 -> ");
 		totalPay(73,150.00);
+
+
 	}
 }
